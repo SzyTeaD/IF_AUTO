378713f3fec86fbaea3f48dd0deece82e59ae19a
yangxin <yangxin@htyunwang.com>
2019-12-24 15:30:45 +0800

    init

--- utils/getToken.py
+++ utils/getToken.py
@@ -5,8 +5,8 @@ from config.pathes import PROJECTINFO
 from utils.FileReader import YamlReader
 
 
-def get_token(project):
-    p = YamlReader(PROJECTINFO).get(project)['token']
+def get_token(project=None):
+    p = YamlReader(PROJECTINFO).get(project)['token'] if project else YamlReader(PROJECTINFO)
     url = p.get('url') if p and p.get('url') else YamlReader(PROJECTINFO).get('token')['url']
     head = p.get('head') if p and p.get('head') else YamlReader(PROJECTINFO).get('token')['head']
     data = p.get('data') if p and p.get('data') else YamlReader(PROJECTINFO).get('token')['data']
@@ -16,6 +16,5 @@ def get_token(project):
 
 
 if __name__ == '__main__':
-    token = get_token('ETPSS')
-    p = YamlReader(PROJECTINFO).get('ETPSS')['token']
+    token = get_token()
     print(token)
