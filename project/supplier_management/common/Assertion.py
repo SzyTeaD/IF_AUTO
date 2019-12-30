2ed2bca448f4847dbaa9dcf7f532a996dd5e1d2e
yangxin <yangxin@htyunwang.com>
2019-12-30 16:24:01 +0800

    init

--- project/supplier_management/common/Assertion.py
+++ project/supplier_management/common/Assertion.py
@@ -6,45 +6,45 @@ class AssertSetIF(object):
         self.project = project
         self.logger = Logger(self.project).get_logger()
 
-    def assertIn(self, member, container):
+    def assertIn(self, member, container, msg=''):
         try:
             if member in container:
-                self.logger.info('测试通过')
+                self.logger.info('测试通过%s' % msg)
             else:
                 raise AssertionError
         except Exception:
             standardMsg = '%s not found in %s' % (member, container)
-            self.logger.error('测试未通过:%s' % standardMsg)
+            self.logger.error('测试未通过:%s%s' % (standardMsg, msg))
 
-    def assertEqual(self, par1, par2):
+    def assertEqual(self, par1, par2, msg=''):
         try:
             if par1 == par2:
-                self.logger.info('测试通过')
+                self.logger.info('测试通过%s' % msg)
             else:
                 raise AssertionError
         except Exception:
             standardMsg = '%s is not %s' % (par1, par2)
-            self.logger.error('测试未通过:%s' % standardMsg)
+            self.logger.error('测试未通过:%s%s' % (standardMsg, msg))
 
-    def assertNotIn(self, member, container):
+    def assertNotIn(self, member, container, msg=''):
         try:
             if member not in container:
-                self.logger.info('测试通过')
+                self.logger.info('测试通过%s' % msg)
             else:
                 raise AssertionError
         except Exception:
             standardMsg = '%s found in %s' % (member, container)
-            self.logger.error('测试未通过:%s' % standardMsg)
+            self.logger.error('测试未通过:%s%s' % (standardMsg, msg))
 
-    def assertNotEqual(self, par1, par2):
+    def assertNotEqual(self, par1, par2, msg=''):
         try:
             if par1 != par2:
-                self.logger.info('测试通过')
+                self.logger.info('测试通过%s' % msg)
             else:
                 raise AssertionError
         except Exception:
             standardMsg = '%s == %s' % (par1, par2)
-            self.logger.error('测试未通过:%s' % standardMsg)
+            self.logger.error('测试未通过:%s%s' % (standardMsg, msg))
 
 
 if __name__ == '__main__':
