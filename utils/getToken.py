8c2b81462b4da972e671f337ad7b67120ad6704d
yangxin <yangxin@htyunwang.com>
2019-12-31 17:00:07 +0800

    init

--- utils/getToken.py
+++ utils/getToken.py
@@ -9,8 +9,8 @@ def get_token(project=None):
     p = YamlReader(PROJECTINFO).get(project)['token'] if project else YamlReader(PROJECTINFO)
     url = p.get('url') if p and p.get('url') else YamlReader(PROJECTINFO).get('token')['url']
     head = p.get('head') if p and p.get('head') else YamlReader(PROJECTINFO).get('token')['head']
-    data = p.get('data') if p and p.get('data') else YamlReader(PROJECTINFO).get('token')['data']
-    r = requests.post(url, data=json.dumps(data), headers=head)
+    boby = p.get('data') if p and p.get('data') else YamlReader(PROJECTINFO).get('token')['data']
+    r = requests.post(url, data=json.dumps(boby), headers=head)
     t = r.json()
     return t['data']['access_token']
 
