73616d8e2871feff04019c3cba907fe54a1fdc57
yangxin <yangxin@htyunwang.com>
2019-12-31 09:24:06 +0800

    init

---
+++ test.py
@@ -0,0 +1,8 @@
+def add1():
+    a = 23
+    b = 34
+    c= a+b
+    return a, b, c
+
+p = add1()
+print(p[1])
--- utils/basePage.py
+++ utils/basePage.py
@@ -9,39 +9,43 @@ from utils.fileReader import ExcelReader
 
 class BasePage(object):
     def __init__(self, file, num=0):
+        """
+        :param file:用例文件名称
+        :param num: 执行用例数量
+        """
         self.num = num
         self.file = file
         self.excel = ExcelReader(self.file)
         self.res = requests
 
-    def workbook(self):
+    def workBook(self):
         # 获取用例
         workbook = self.excel.data[self.num]
         return workbook
 
     def get_title(self):
         # 获取标题，默认第一行
-        title = self.workbook().get('用例标题')
+        title = self.workBook().get('用例标题')
         return title
 
     def url_adress(self):
         # 获取url地址，默认第一行
-        url_adress = self.workbook().get('前置条件')
+        url_adress = self.workBook().get('前置条件')
         return url_adress
 
     def params(self):
         # 获取参数，默认第一行
-        params = self.workbook().get('步骤')
+        params = self.workBook().get('步骤')
         return ast.literal_eval(params)
 
     def expected_results(self):
         # 读取预期结果，默认第一行
-        expected_results = self.workbook().get('预期')
+        expected_results = self.workBook().get('预期')
         return ast.literal_eval(expected_results)
 
     def get_method(self):
         # 获取方法
-        method = self.workbook().get('关键词')
+        method = self.workBook().get('关键词')
         return method
 
     def max_rows(self):
